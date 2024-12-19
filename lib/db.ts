0b76d85a383741239da127938991624e0cb04048
Michael Bamikunle <mikulobams@gmail.com>
2024-12-19 21:53:33 +0000

    Added connect db

---
+++ lib/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connect = async () => {
+  const connectionState = mongoose.connection.readyState;
+
+  if (connectionState === 1) {
+    console.log("DB is already connected");
+    return;
+  }
+
+  if (connectionState === 2) {
+    console.log("DB is connecting");
+    return;
+  }
+
+  try {
+    mongoose.connect(MONGODB_URI!, {
+      dbName: "next-mongodb-restapis",
+      bufferCommands: true,
+    });
+    console.log("DB connected");
+  } catch (error: any) {
+    console.log("Error: ", error);
+    throw new Error("Error: ", error);
+  }
+};
+
+export default connect;
