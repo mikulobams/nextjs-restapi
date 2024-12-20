992d32b52eceda817a5af7715592d61285283102
Michael Bamikunle <mikulobams@gmail.com>
2024-12-20 20:11:13 +0000

    Add PATCH and DELETE methods for user management; create Category model

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,6 +1,11 @@
 import connect from "@/lib/db";
-import User from "@/lib/modals/users";
+import User from "@/lib/models/users";
+
+import { Types } from "mongoose";
 import { NextResponse } from "next/server";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try {
@@ -28,6 +33,102 @@ export const POST = async (req: Request) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return new NextResponse("Error creating user " + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const PATCH = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { userId, newUsername } = body;
+    await connect();
+
+    if (!userId || !newUsername) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or new username not found" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid ID" }), {
+        status: 400,
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { username: newUsername },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is updated", user: updatedUser }),
+      { status: 200 }
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return new NextResponse("Error updating user " + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return new NextResponse(JSON.stringify({ message: "ID not found" }), {
+        status: 400,
+      });
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid ID" }), {
+        status: 400,
+      });
+    }
+
+    await connect();
+
+    const deletedUser = await User.findOneAndDelete({
+      _id: new ObjectId(userId),
+    });
+
+    if (!deletedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is deleted", user: deletedUser }),
+      {
+        status: 200,
+      }
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return new NextResponse("Error deleting user " + error.message, {
       status: 500,
     });
   }
