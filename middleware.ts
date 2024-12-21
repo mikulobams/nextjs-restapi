bf62cb044105492e711e1827205333f3d71ddfa7
Michael Bamikunle <mikulobams@gmail.com>
2024-12-21 15:28:06 +0000

    Add authentication and logging middleware for API requests; implement token validation

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,21 @@
 import { NextResponse } from "next/server";
+import { authMiddleware } from "@/middlewares/api/authMiddleware";
+import { logMiddleware } from "./middlewares/api/logMiddleware";
 
 export const config = {
   matcher: "/api/:path*",
 };
 
 export default function middleware(req: Request) {
+  if (req.url.includes("/api/blogs")) {
+    const logResult = logMiddleware(req);
+    console.log(logResult.response);
+  }
+  const authResult = authMiddleware(req);
+  if (!authResult?.isValid) {
+    return new NextResponse(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
   return NextResponse.next();
 }
